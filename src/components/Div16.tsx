343e655cec5ff293aa15973def2f97db6e47e1ec
devmaniel <devmaniel@gmail.com>
2025-07-11 07:56:20 +0800

    update the responsive

--- src/components/Div16.tsx
+++ src/components/Div16.tsx
@@ -14,7 +14,7 @@ const Div16 = () => {
       />
 
       <div className="absolute inset-0 flex items-center justify-center z-50">
-        <h1 className="lg:text-4xl md:text-2xl text-2xl font-black text-white">Where design feels, code thinks, and vision leads.</h1>
+        <h1 className="lg:text-4xl md:text-2xl text-sm p-5 text-center font-black text-white">Where design feels, code thinks, and vision leads.</h1>
       </div>
     </>
   );
